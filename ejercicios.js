22ad27d002580222af60a66f03a829daf26e8e98
Andrés O <aforozco556340@umanizales.edu.co>
2022-05-01 20:03:02 -0500

    Ejercicios JavaScript

---
+++ ejercicios.js
@@ -0,0 +1,22 @@
+
+//Ejercicio1
+
+
+
+//Ejercicio2
+let numDec=parseFloat(prompt("Ingrese un número decimal: "));
+let numInt=parseInt(prompt("Ingrese un número Entero"));
+let sumTot=parseFloat(numDec+numInt);
+
+console.log(`Número decimal: ${numDec}\nNúmero entero: ${numInt}\nSuma Total: ${sumTot}`);
+
+//Ejercicio3
+
+
+
+//Ejercicio4
+let kmReco=parseFloat(prompt("Ingrese los KM recorridos por la moo: "));
+let litCombus=parsefloat(prompt("Ingrese los litros de combustible consumidos: "));
+let consumkm=parseFloat(kmReco/litCombus);
+
+console.log(`Número decimal: ${numDec}\nNúmero entero: ${numInt}\nSuma Total: ${sumTot}`);
