4aec801469f13bc981e12df613442a4ea783b02f
Andrés O <aforozco556340@umanizales.edu.co>
2022-05-03 18:59:26 -0500

    Se actualiza readme.md

--- ejercicio_2.js
+++ ejercicio_2.js
@@ -1,6 +1,6 @@
-var decimal = parseFloat(prompt("Ingrese un numero decimal: "));
-var entero = parseInt(prompt("Ingrese un numero entero: "));
-var suma = decimal+entero
+let decimal = parseFloat(prompt("Ingrese un numero decimal: "));
+let entero = parseInt(prompt("Ingrese un numero entero: "));
+let suma = decimal+entero
 console.log("El primer numero es: " +decimal);
 console.log("El segundo numero es: " +entero);
 console.log("El resultado de la suma es: " +suma);
