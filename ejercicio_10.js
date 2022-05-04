e05e4ba293c70ef339bb3a53becb17c2239a4c6f
Andrés O <aforozco556340@umanizales.edu.co>
2022-05-03 19:41:18 -0500

    Se actualiza readme.md

--- ejercicio_10.js
+++ ejercicio_10.js
@@ -1,10 +1,10 @@
 let show = (prompt("Cuantos Shows musicales a visto en el último año?: "));
-console.log(" Shows vistos en el ultimo año: " +show)
+console.log(" Shows vistos en el ultimo año: " +show);
 
 if (show <=3){
-  console.log(false)   
+  console.log(false);   
 }else{
-  console.log(true)  
+  console.log(true);  
 } 
 
 
--- ejercicio_7.js
+++ ejercicio_7.js
@@ -1,4 +1,4 @@
-var numero = parseFloat(prompt("Ingrese el un número: "));
+let numero = parseFloat(prompt("Ingrese el un número: "));
 console.log("El número ingresado es: "+ numero);
-var por100 = (numero-(numero*0.15))
+let por100 = (numero-(numero*0.15))
 console.log("descontando el 15% queda : "+ por100);
